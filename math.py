ddb427fc080cc62b316d33b01774312f604ffcc8
foxxer <coluna123@protonmail.ch>
2021-03-06 19:38:27 -0300

    added unit testing

--- math.py
+++ math.py
@@ -1,3 +1,5 @@
+import unittest
+
 def fact(n, accumulator=1):
     """
     TAIL RECURSION
@@ -22,3 +24,23 @@ def combinate(l, p):
     p - number of possibilities
     """
     return p ** l
+
+class SuitCase(unittest.TestCase):
+    def test_factorial(self):
+        self.assertEqual(fact(5), 5 * 4 * 3 * 2 * 1)
+    def test_permutate(self):
+        """
+        Imagine a scenario where I am gonna choose the five best
+        workers of the month, how can I calculate how much combinations
+        can it derive from?
+        """
+        self.assertEqual(permutate(30, 5), 30 * 29 * 28 * 27 * 26)
+    def test_combination(self):
+        """
+        Imagine a scenario where I am gonna calculate how many numbers I can write
+        on a paper that requires three numbers
+        """
+        self.assertEqual(combinate(3, 10), 10 * 10 * 10)
+
+if __name__ == "__main__":
+    unittest.main()
