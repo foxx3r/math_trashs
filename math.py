8f7bce840821ada7dd16274d59e6b8d27403ed24
foxxer <coluna123@protonmail.ch>
2021-03-06 20:09:06 -0300

    added multiples combinators, adapted to PEP's standard, imported func tools and made head and tail function

--- math.py
+++ math.py
@@ -1,4 +1,18 @@
 import unittest
+from functools import *
+
+
+def head(x):
+    if len(x) == 0:
+        return []
+    return x[0]
+
+
+def tail(xs):
+    if len(xs) < 1:
+        return []
+    return xs[1:]
+
 
 def fact(n, accumulator=1):
     """
@@ -11,12 +25,14 @@ def fact(n, accumulator=1):
     else:
         return fact(n - 1, accumulator * n)
 
+
 def permutate(p, n):
     """
     p - object to be permutated
     n - the length of fields (by elimination)
     """
     return int(fact(p) / fact(p - n))
+
 
 def combinate(l, p):
     """
@@ -25,9 +41,24 @@ def combinate(l, p):
     """
     return p ** l
 
+
+def combinate_multiples(l, p):
+    """
+    l - length of object
+    p - list of possibilities for each field
+
+    WARNING
+    It's very important that you do not put 0 in the list p
+    """
+    if l != len(list(p)):
+        raise "1st argument must be equal to the length of the 2nd argument"
+    return reduce(lambda x, y: x * y, p, 1)
+
+
 class SuitCase(unittest.TestCase):
     def test_factorial(self):
         self.assertEqual(fact(5), 5 * 4 * 3 * 2 * 1)
+
     def test_permutate(self):
         """
         Imagine a scenario where I am gonna choose the five best
@@ -35,12 +66,14 @@ class SuitCase(unittest.TestCase):
         can it derive from?
         """
         self.assertEqual(permutate(30, 5), 30 * 29 * 28 * 27 * 26)
+
     def test_combination(self):
         """
-        Imagine a scenario where I am gonna calculate how many numbers I can write
-        on a paper that requires three numbers
+        Imagine a scenario where I am gonna calculate how many numbers I can
+        writen on a paper that requires three numbers
         """
         self.assertEqual(combinate(3, 10), 10 * 10 * 10)
+
 
 if __name__ == "__main__":
     unittest.main()
